73f8ce89ffabc29b44df3821f588b1bed764b1e3
Shirokun <khoirul.20998@gmail.com>
2022-06-21 00:17:16 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Image, StatusBar, Text, TouchableOpacity, View } from 'react-native';
+import AzureAuth from 'react-native-azure-auth';
+
+const CLIENT_ID = 'isi dengan client id kalian'; // replace the string with YOUR client ID
+
+const azureAuth = new AzureAuth({
+    clientId: CLIENT_ID
+});
+
+const App = () => {
+
+    const [state, setState] = React.useState({
+        accessToken: null,
+        user: '',
+        userId: ''
+    });
+
+    const _onLogin = async () => {
+        try {
+            let tokens = await azureAuth.webAuth.authorize({ scope: 'openid profile User.Read' })
+            console.log('CRED>>>', tokens)
+            getInfo(tokens);
+        } catch (error) {
+            console.log('Error during Azure operation', error)
+        }
+    };
+
+    const getInfo = async (tokens) => {
+        try {
+            let info = await azureAuth.auth.msGraphRequest({ token: tokens.accessToken, path: 'me' })
+            console.log('info', info)
+            setState({ user: info.displayName, userId: tokens.userId, accessToken: tokens.accessToken })
+        } catch (error) {
+            console.log('Error during Azure operation', error)
+        }
+    }
+
+    const _onLogout = () => {
+        azureAuth.webAuth
+          .clearSession({
+            closeOnLoad: true,
+          })
+          .then(success => {
+            this.setState({ accessToken: null, user: null });
+          })
+          .catch(error => console.log(error));
+      };
+
+    return (
+        <View style={{
+            backgroundColor: '#0b5aa4',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <StatusBar
+                backgroundColor={'#0b5aa4'}
+            />
+            <View style={{
+                width: 160,
+                height: 160,
+                backgroundColor: 'white',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginBottom: 200,
+                borderRadius: 160,
+            }}>
+                <Image
+                    source={require('./azure.png')}
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain',
+                    }}
+                />
+            </View>
+            <TouchableOpacity style={{
+                backgroundColor: 'white',
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                borderRadius: 5,
+                flexDirection: 'row',
+                alignItems: 'center',
+            }} onPress={() => state.accessToken ?  _onLogout() : _onLogin()}>
+                <Image
+                    source={require('./azure.png')}
+                    style={{
+                        width: 30,
+                        height: 30,
+                        resizeMode: 'contain',
+                        marginRight: 10,
+                    }}
+                />
+                <Text style={{
+                    color: '#3ac5f2',
+                    fontWeight: 'bold',
+                    fontSize: 30,
+                }}>{state.accessToken ? "Logout" : "Login"} Azure</Text>
+            </TouchableOpacity>
+        </View>
+    )
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,5 @@
+Perhatikan dulu sebelum mencoba
+
+pastikan di directory <b>android/app/src/main/AndroidManifest.xml</b> ganti bagian <b>com.testloginazure</b> dengan package app name kalian.
+
+lalu baca lagi <a href="https://github.com/vmurin/react-native-azure-auth" target="_blank">di sini</a> 
